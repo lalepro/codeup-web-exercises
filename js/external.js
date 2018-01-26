39b14a9820347a52a9db6e8ad5d8868e6401da58
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-01-26 09:07:38 -0600

    JS

---
+++ js/external.js
@@ -0,0 +1,99 @@
+"use strict";
+
+console.log('Hello from JavaScript!');
+
+alert('Welcome to my Website!');
+
+var userInput = prompt('What is your favorite color?');
+console.log('The user entered favorite color of' + ' ' + userInput);
+
+var confirmed =confirm('Are you sure' + " " + userInput + " " + "is your favorite color?");
+alert("Awesome!!!" + " " + userInput + " " + "is my favorite color too!");
+
+
+
+// function myFunction(input) {
+//     var output = ;
+// }   return output;
+
+
+// someFunction();
+// parseInt("42");
+//
+// var result = doSomething();
+// console.log(doSomething());
+//
+// var myString = "42";
+// var myNumber = parseInt(myString);
+
+
+
+// function myFunction(input) {
+//     var output = ;
+// }   return output;
+
+
+
+// function increment(x) {
+//     return x + 1;
+// }
+//
+// var four = increment(3);
+// var six = increment(increment(increment(3)));
+
+
+// var six = increment(increment(increment(3)));
+// var six = increment(increment(4));
+// var six = increment(5);
+// var six = 6;
+
+// function increment(someNumber) {
+//     return someNumber + 1;
+//     console.log('you will never see this.');
+//     return someNumber + 2;
+// }
+
+// var increment = function(x) {
+//     return x + 1;
+// }
+//
+// var two = increment(1);
+
+//********************
+
+// function sum(a, b) {
+//     var result = a + b;
+// }
+//
+// var x = 3;
+// var seven = sum(x,4);
+
+
+//
+// function shout(message) {
+//     alert(message.toUpperCase() + "!!!!")
+// }
+//
+// var returnValue = shout('hello there');
+// console.log(returnValue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
