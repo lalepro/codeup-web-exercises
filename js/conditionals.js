94d69233d84007e40b6567d9e669e8ef3ff98766
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-01-29 16:28:28 -0600

    exercise finished, still working on bonuses

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,40 +1,40 @@
 "use strict";
-//
-//
-// window.confirm("Confirm you'd like to enter a number")
-//     if (confirm) {
-//     var response = prompt("Please input a number:");
-//     evenOdd(response); positiveNegative(response); hundreth(response);
-//     }
-//     else {
-//     console.log("Bye");
-//     }
-//
-// function evenOdd(response) {
-//     var reminder = response % 2;
-//     if (reminder == 0) {
-//         alert("Your number is even");
-//     } else if (reminder == Math.round(reminder)) {
-//         alert("Your number is odd");
-//     } else {
-//         alert("Bye!");
-//     }
-// }
-//
-//
-// function positiveNegative(response) {
-//     if (response >= 0) {
-//         alert("Your number is positive!");
-//     } else {
-//         alert("Your number is negative!");
-//     }
-// }
-//
-// function hundreth(response) {
-//     var hundredMore = +response + +100;
-//     alert("You input plus 100 equals:" + hundredMore);
-//
-// }
+
+
+window.confirm("Confirm you'd like to enter a number")
+    if (confirm) {
+    var response = prompt("Please input a number:");
+    evenOdd(response); positiveNegative(response); hundreth(response);
+    }
+    else {
+    console.log("Bye");
+    }
+
+function evenOdd(response) {
+    var reminder = response % 2;
+    if (reminder == 0) {
+        alert("Your number is even");
+    } else if (reminder == Math.round(reminder)) {
+        alert("Your number is odd");
+    } else {
+        alert("Bye!");
+    }
+}
+
+
+function positiveNegative(response) {
+    if (response >= 0) {
+        alert("Your number is positive!");
+    } else {
+        alert("Your number is negative!");
+    }
+}
+
+function hundreth(response) {
+    var hundredMore = +response + +100;
+    alert("Your input plus 100 equals: " + hundredMore);
+
+}
 
 
 
@@ -56,35 +56,39 @@
 
 /* ########################################################################## */
 
-// window.confirm("Do you have a favorite color?")
-//     if (confirm) {
-//     var colors = prompt("What is your favorite color?");
 
 
-function analyzeColor(colors) {
+window.confirm("Do you have a favorite color?")
+    if (confirm) {
+        var colors = prompt("What is your favorite color?");
 
-    if (colors === 'red') {
-        console.log("Red is the color of a beating heart");
+
+        function analyzeColor(colors) {
+            var message;
+
+            if (colors === 'red') {
+                message = "Red is the color of a beating heart";
+            } else if (colors === 'blue') {
+                message = "Blue is the color of the sky";
+            } else if (colors === 'orange') {
+                message = "Orange is the color of fruits in Florida";
+            } else if (colors === 'yellow') {
+                message = "Yellow is a terrible color";
+            } else if (colors === 'green') {
+                message = "Green is the color of grass";
+            } else if (colors === 'indigo') {
+                message = "Old Lesbos love indigo girls";
+            } else if (colors === 'violet') {
+                message = "Violet is the color of some flowers";
+            } else {
+                message = "No colors for you."
+            }
+            return message;
+        }
     }
-    else  if (colors === 'blue') {
-            console.log("Blue is the color of the sky");
-        }
-    else if (colors === 'orange') {
-        console.log("Orange is the color of fruits in Florida");
-        }
-    else if (colors === 'yellow') {
-        console.log("Yellow is a terrible color");
-        }
-    else if (colors === 'green') {
-        console.log("Green is the color of grass");
-        }
-    else if (colors === 'indigo') {
-        console.log('indigo girls');
-    }
-    else if (colors === 'violet') {
-            console.log('Violet is the color of some flowers');
-        }
-}
+
+    alert(analyzeColor(randomColor));
+
 
 /**
  * TODO:
@@ -118,6 +122,42 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  */
 
 
+function analyzeColor(colors) {
+
+    switch(colors) {
+        case 'red':
+        return "Red is the color of a beating heart";
+        break;
+
+        case 'blue':
+        return "Blue is the color of the sky";
+        break;
+
+        case 'orange':
+        return "Orange is the color of fruits in Florida";
+        break;
+
+        case 'yellow':
+        return "Yellow is a terrible color";
+        break;
+
+        case 'green':
+        return "Green is the color of grass";
+        break;
+
+        case 'indigo':
+        return 'Old school lesbos love indigo girls';
+        break;
+
+        case 'violet':
+        return 'Violet is the color of some flowers';
+    }
+}
+
+
+
+console.log(analyzeColor(randomColor));
+
 
 /**
  * TODO:
@@ -132,6 +172,57 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  */
 
 /* ########################################################################## */
+
+// Generate a random number between 0 and 6
+
+
+var luckyNumber = Math.floor(Math.random() * 6);
+
+
+
+window.confirm("Press okay to get your random lucky number");
+        alert("Your lucky number is " + luckyNumber);
+
+
+
+
+calculateTotal(luckyNumber);
+
+
+function calculateTotal(number) {
+    var bill = prompt("What is your bill total?");
+
+    if (number === 1) {
+        var due = bill - bill * 0.10;
+        alert("You owe" + " " + "$" + due);
+    }
+
+    else if (number === 2) {
+        var due = bill - bill * 0.25;
+        alert("You owe" + " " + "$" + due);
+    }
+
+    else if (number === 3) {
+        var due = bill - bill * 0.35;
+        alert("You owe" + " " + "$" + due);
+    }
+
+    else if (number === 4) {
+        var due = bill - bill * 0.50;
+        alert("You owe" + " " + "$" + due);
+    }
+    else if (number === 5) {
+        var due = bill - bill;
+        alert("You owe nothing!! Everything is free!!");
+    } else {
+        var due = bill;
+        alert("You have NO LUCK!! Sorry, you owe" + " " + "$" + due);
+
+    }
+
+}
+
+
 
 /**
  * TODO:
@@ -160,5 +251,49 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * and alerts to display to the user what their lucky number was, what their
  * price before the discount was, and what their price after the discount is.
  */
-// Generate a random number between 0 and 6
-// var luckyNumber = Math.floor(Math.random() * 6);
+
+//*********************************************************************************
+//
+//         = CONDITIONALS BONUSES
+//         Bonus 1.
+//         Write a function (or multiple functions) that will return
+//         a boolean depending on if the string value passed to it
+//         as an argument is the name of a day of the week that starts with a T.
+//             Otherwise, it should return false.
+//             Example: dayOfTheWeekStartsWithT("Monday")     => returns false
+//         Example: dayOfTheWeekStartsWithT("Tuesday")     => returns true
+//         Example: dayOfTheWeekStartsWithT("Tommy")     => returns false
+//
+//         Bonus 2.
+//         Write a function, isValidPassword, that takes in a string argument and returns true or false
+//         depending on whether or not all the following conditions are true:
+//         1) Must be 6 characters long (only for the sake of this exercise, NOT a best practice)
+// 2) Contains at least one letter and one number
+// 3) Contains at least one upper and one lower case letter
+// 4) Only comprised of letters and numbers
+// 5) EXTRA BONUS: is not the same forwards and backwards
+
+var string = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+function dayOfTheWeekStartsWithT(string) {
+
+    switch (string) {
+                case
+                string[0] === "T" + string.substring(1):
+                    return true;
+                    break;
+
+                case
+                string[0] !== "T" + string.substring(1) :
+                    return false;
+                    break;
+
+        case string: "Tuesday" || "Thursday";
+            return true;
+            break;
+
+        case string: ;
+            return false;
+
+            }
+        }
