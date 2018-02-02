0c46341e3652d615745298cc5722dde0eb913c15
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-02-02 14:58:42 -0600

    update

---
+++ js/House-Warming.js
@@ -0,0 +1,56 @@
+'use-strict';
+
+
+var houseneeds = [
+    {
+        wants: {
+            darby: {
+                location: "walkable to hipster stuff",
+                price: "affordable",
+                safe: "security system",
+                design: "high ceilings or open area",
+                bathroom: "need 2 & modern design",
+                maintenance: "none to low yard work"
+            },
+            laura: {
+                bones: "solid foundation, built on rock not sand",
+                safety: "neighbors friendly and nice",
+                location: "buy where our property will increase in value",
+                view: "can see sun set or rise",
+                amenities: {
+                    bedrooms: "3 or more",
+                    bathroom: "2",
+                    air: "centeral ac",
+                    yard: "garage, backyard patio,",
+                    kitchen: "dishwasher",
+                    laundry: "washer & dryer hookups"
+                }
+
+                }
+            }
+        dont: {
+            darby: {
+                pricy: "don't want to spend over 200k",
+                location: "far away from downtown"
+            },
+            laura: {
+                moneyPit: "don't want to be throwing extra money into house",
+                foundation: "foundation issues",
+                tiny: "need space from neighbors",
+                air: "window ACs",
+                danger: "high crime neighborhood"
+
+            }
+        }
+
+        }
+];
+
+
+
+// wants: affordable, near stuff, walkable to fun things.
+//     No need for lawn mower.  Open area - dont need open
+// concept but do like higher ceilings.
+//     Some security system thats easy to maintain.
+//     If bathroom is ugly, then we need two so we
+// can rehab one at a time.
