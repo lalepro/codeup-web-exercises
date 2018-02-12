1bb49bcc1c67da7883661c5272541b3591cf56c2
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-02-12 09:27:54 -0600

    save JS query

--- js/for_loops.js
+++ js/for_loops.js
@@ -58,3 +58,129 @@ for(var i = 100; i >= 5; i -=5) {
 
 
 //_____________________________________________________________
+
+//
+// LOOPS BONUSES
+//
+// BONUSES 1:
+//
+// Write a function to toggle all characters in a string: in a
+// given string, toggle all characters to their opposite case.
+// Special characters will not change.
+//
+//     Example input: “aBc12#”
+// Example output: “AbC12#”
+
+
+function toggle(string){
+        var str = (" ");
+        for(var i = 0; i < string.length; i++){
+             var change = string[i];
+            if(change == change.toUpperCase()) {
+                str += change.toLowerCase();
+            } else if (change.toLowerCase()){
+                str += change.toUpperCase();
+            } else {
+                str += change;
+            }
+        }
+            return str;
+}
+
+//-------_--_-_----_-_----_---------_--_OR
+
+str = 'Hi, Stack Overflow.';
+res = '';
+for (var i = 0; i < str.length; ++i) {
+    c = str[i];
+    if (c == c.toUpperCase()) {
+        res += c.toLowerCase();
+    } else if (c == c.toLowerCase()) {
+        res += c.toUpperCase();
+    } else {
+        res += c;
+    }
+}
+
+
+// BONUS 2:
+//
+// Write a function that takes a given string appended
+// with a number at the end. The program will check the length of
+// the string to verify the string is equal to the number appended.
+// If the number appended equals the string length, output “Yes”. if
+// the number appended does not equal the string length, output “No”.
+//
+// Example Input: “geek5”
+// Example Output: “Yes”
+//
+// Example Input: “codingchallenge25”
+// Example Output: “No”
+
+
+
+function insane(str){
+    var n = str.length();
+    var num = 0;
+    var x = 1;
+    var i = n-1;
+    for (i=n-1; i>=0; i--) {
+        if ('0' <= str[i] &&  str[i]  <= '9') {
+            num = (str[i] - '0')*x + num;
+            x = x * 10;
+        }
+        else break;
+    }
+
+    return num == i + 1;
+}
+
+
+
+//longest word returns length of string:
+
+function findLongestWord(str) {
+    var strSplit = str.split(' ');
+    var longestWord = 0;
+    for(var i = 0; i < strSplit.length; i++){
+        if(strSplit[i].length > longestWord){
+            longestWord = strSplit[i].length;
+        }
+    }
+    return longestWord.length;
+}
+findLongestWord("The quick brown fox jumped over the lazy dog");
+
+
+
+//longest word of a string, returns the word:
+
+function LongestWord(sen) {
+    sen=sen.replace(/[^a-zA-Z ]/ig,'')
+    words = sen.split(" ")
+    LongestWord = words[0]
+    for (i = 0; i<words.length; i ++) {
+        if (words[i].length > LongestWord.length) {
+            LongestWord = words[i]
+        }
+    }
+    return LongestWord
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/objects.js
+++ js/objects.js
@@ -502,8 +502,17 @@ function countVowel(string) {
 
 
 
+//    factorial function
 
 
+    function factorial(num) {
+        if (num ===0 || num === 1)
+            return 1;
+        for (var i = num - 1; i >= 1; i--) {
+            num *= i;
+        }
+        return num;
+    } ​
 
 
 
